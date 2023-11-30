517a50d707d826f2b713c63a199479aa3eca5be9
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2023-11-30 15:21:08 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,6 +10,5 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     expect(1, 1);
-    //test
   });
 }
